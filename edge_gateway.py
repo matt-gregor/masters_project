2fc4a9d86b8e7e6f923a557218bcc11954aeaea5
Mateusz <greenwaild@gmail.com>
2023-06-24 21:44:38 +0200

    testing connection between mqtt broker and cloud

--- edge_gateway.py
+++ edge_gateway.py
@@ -1,12 +1,22 @@
 import random
+import time
 
+import requests
 from paho.mqtt import client as mqtt_client
 
 """
 TODO:
 -encrypted storage of username & password
+-communication with cloud
+-deconstruction of data frame and sending it to the plc
 -non blocking communication with cloud
 """
+
+# Define the URL of the endpoint
+url = 'http://16.16.220.162:8080/your-endpoint'  # cloud
+# url = 'http://127.0.0.1:8080/your-endpoint'    # on-premise
+# Define the data to send in the request body
+
 broker = '192.168.1.100'
 port = 1883
 topic = "siemens"
@@ -32,8 +42,23 @@ def connect_mqtt() -> mqtt_client:
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         # mess = [float(part) for part in msg.payload.decode().split()]
-        print("Message content: " + str(msg.payload.decode()))
-        publish(client, "other_topic", msg.payload.decode())
+        # print("Message content: " + str(msg.payload.decode()))
+        # publish(client, "other_topic", msg.payload.decode())
+
+        data = {
+            'value': 15
+        }
+        response = requests.post(url, json=data)
+        # Check the response status code
+        if response.status_code == 200:
+            # Request was successful
+            result = response.json()
+            print('Result:', result)
+            publish(client, "other_topic", str(result))
+        else:
+            # Request encountered an error
+            print('Error:', response.status_code, response.json())
+
     client.subscribe(topic)
     client.on_message = on_message
 
