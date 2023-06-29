be4f07f413d9d5f25c743b9982c6e0b6e547e036
Mateusz <greenwaild@gmail.com>
2023-06-30 00:25:19 +0200

    adrc + tests

--- adrc_test.py
+++ adrc_test.py
@@ -6,14 +6,19 @@ b0 = 0.26816115942028985507246376811594
 delta = 0.1
 order = 1
 t_settle = 0.5714
-k_eso = 6
-adrc_statespace = pyadrc.StateSpace(order, delta, b0, t_settle, k_eso, r_lim=(0, 4), m_lim=(0, 4))
+k_eso = 10
+adrc_statespace = pyadrc.StateSpace(order, delta, b0, t_settle, k_eso, m_lim=(0, 4), r_lim=(-1, 1))
 setpoint = 3
 ctrl = 0.
 
 
 pv = 2
-for i in range(100):
+for i in range(10):
 
     ctrl = adrc_statespace(pv, ctrl, setpoint)
+    print(ctrl)
+print("a")
+for i in range(10):
+
+    ctrl = adrc_statespace(4, ctrl, setpoint)
     print(ctrl)
