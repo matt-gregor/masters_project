8addd2c53fedca8f21bdf5a478728fdc89848bc6
Mateusz <greenwaild@gmail.com>
2023-07-12 22:18:19 +0200

    some bs

--- mqtt_test.py
+++ mqtt_test.py
@@ -3,19 +3,21 @@ import time
 
 import requests
 
-data = [(1, 1)]
+# data = [(1, 1)]
 
-url = 'http://127.0.0.1:1880/aaao'
-session = requests.Session()
-session.get(url)
+# url = 'http://127.0.0.1:1880/aaao'
+# session = requests.Session()
+# session.get(url)
 
 
-async def post(url, data):
-    response = requests.post(url, data=data)
-    return response
+# async def post(url, data):
+#     response = requests.post(url, data=data)
+#     return response
 
-for i in range(100):
-    time1 = time.perf_counter_ns()
-    asyncio.run(post(url, data))
-    time2 = (time.perf_counter_ns() - time1)/1000000
-    print(f"{time2}")
+# for i in range(100):
+#     time1 = time.perf_counter_ns()
+#     asyncio.run(post(url, data))
+#     time2 = (time.perf_counter_ns() - time1)/1000000
+#     print(f"{time2}")
+
+print(None - None)
